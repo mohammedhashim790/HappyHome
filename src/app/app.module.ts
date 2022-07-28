a35296f7c4a92c61353d236fd501ccbe88932325
mohammedhashim790 <55325133+mohammedhashim790@users.noreply.github.com>
2022-07-28 22:46:14 +0530

    EMI AND ELIGIBILTY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./Views/home/home.component";
 import {SignInComponent} from "./Views/sign-in/sign-in.component";
+import {FloatingWidgetComponent} from "./Views/Widgets/floating-widget/floating-widget.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent
+    SignInComponent,
+    FloatingWidgetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
