fefe66e8a710ed9cfd7b3480f74a51fed3976462
SathwikaThummalapalli <sathwikathummalapalli428@gmail.com>
2022-07-30 10:03:25 +0530

    signup and loandetail2 committed

---
+++ src/app/Views/loandetail2/loandetail2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loandetail2',
+  templateUrl: './loandetail2.component.html',
+  styleUrls: ['./loandetail2.component.css']
+})
+export class Loandetail2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./Views/home/home.component";
 import {SignInComponent} from "./Views/sign-in/sign-in.component";
+import { Loandetail2Component } from './Views/loandetail2/loandetail2.component';
+import {SignUpComponent} from './Views/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent,
+    Loandetail2Component
   ],
   imports: [
     BrowserModule,
